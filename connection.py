0f830bad4466c0cdece431c45d667e70157da1f6
AgtOwad <ayushau77@gmail.com>
2025-07-19 20:04:51 +0100

    Updated Cloud-DB Acess

--- connection.py
+++ connection.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 import os
+from urllib.parse import quote_plus
 
 load_dotenv()
 
 DB_USER     = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
 DB_HOST     = os.getenv("DB_HOST")
 DB_PORT     = os.getenv("DB_PORT")
 DB_NAME     = os.getenv("DB_NAME")
